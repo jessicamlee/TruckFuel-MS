870a75c40a88ae9e164c3c00e3a0c4c9cf0dae7f
jessicamlee <jessicamlee25@gmail.com>
2023-01-10 21:46:36 -0800

    :fire: :art: Setup Gulp for styles files
    - Use SASS to reorganize styles files

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,29 @@
 const { src, dest, watch, series } = require('gulp');
-// const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
 
-// function build_css() {
+function build_css() {
 
-//     return src('./src/sass/**/main.scss')
-//         // Add source files into respective (sub)folders
+    return src('./src/styles/**/main.scss')
 
-//         // Include the pipe command for sourcemaps
-//         .pipe(sourcemaps.init())
+        // Include the pipe command for sourcemaps
+        .pipe(sourcemaps.init())
 
-//         // Build Sass, log the errors if there are any errors
-//         .pipe(sass({
-//             // minify/compress the sass file by defining the outputStyle
-//             outputStyle: 'compressed'
-//         }).on('error', sass.logError))
+        // Build Sass, log the errors if there are any errors
+        .pipe(sass({
+            // minify/compress the sass file by defining the outputStyle
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
 
-//         // Print the sourcemap into the same main.css file
-//         .pipe(sourcemaps.write())
+        // Print the sourcemap into the same main.css file
+        .pipe(sourcemaps.write())
 
-//         // Build into the build/css folder
-//         .pipe(dest('./build/css/'));
+        // Build into the build/css folder
+        .pipe(dest('./build/styles/'));
     
-// }
+}
 
 function build_js() {
     // Files are concatenated in the order that they would be printed in the destination folder/file
@@ -45,10 +44,10 @@ function build_js() {
 };
 
 function watchTasks() {
-    // watch('./src/sass/**/*.scss', build_css);
+    watch('./src/styles/**/*.scss', build_css);
     watch('./src/scripts/**/*.js', build_js);
 }
 
 // Add 'exports' for gulp to perform the series of functions
-exports.default = series(build_js, watchTasks);
-// exports.default = series(build_css, build_js, watchTasks);
+// exports.default = series(build_js, watchTasks);
+exports.default = series(build_css, build_js, watchTasks);
